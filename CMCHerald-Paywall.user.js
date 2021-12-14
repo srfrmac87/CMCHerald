fc941a2d68f95cdbff4103c885422f50000789d0
srfrmac87 <48164725+srfrmac87@users.noreply.github.com>
2021-12-14 11:13:03 -0500

    Update CMCHerald-Paywall.user.js
    Updated for changes to paywall on site

--- CMCHerald-Paywall.user.js
+++ CMCHerald-Paywall.user.js
@@ -1,14 +1,11 @@
 // ==UserScript==
 // @name         Herald Fixer
 // @namespace    http://tampermonkey.net/
-// @version      0.52
-// @description  try to take over the world!
+// @version      0.54
+// @description  Updated 12/14/21 for new paywall
 // @author       You
 // @match        https://www.capemaycountyherald.com/*
 // @icon         https://www.google.com/s2/favicons?domain=capemaycountyherald.com
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_deleteValue
 // @grant        GM_addStyle
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://github.com/srfrmac87/CMCHerald/raw/main/CMCHerald-Paywall.user.js
@@ -19,12 +16,6 @@
     'use strict';
 
     var $ = window.jQuery;
-    // This was original event listener:
-    //     window.addEventListener ("load", timeOutFunction, false);
-    //     function timeOutFunction () {
-    //         setTimeout(Greasemonkey_main, 1500);
-    //     }
-
     var timerVar = setInterval (function() {Greasemonkey_main (); }, 125);
     var trueA = false;
     var trueB = false;
@@ -32,40 +23,30 @@
     var counter = 0;
 
     function Greasemonkey_main () {
-        if($('#promo-designer-modal-custom-pop').length){
-            $('button.close').click();
+        if($('div#access-offers-modal').length){
+            $('div#access-offers-modal').css("display", "none"); // this removes paywall box ****updated in .54 for problems found on 12/14/21****
             trueA=true;
-            //alert("A length = " + $('body.tp-modal-open').length);
         }
-        if($('#subscription-modal').length){
-            $('button.close').click();
+        if($('div.modal-backdrop.fade.in').length){
+            $('div.modal-backdrop.fade.in').css("display", "none"); // this removes transparency overlay ****updated in .54 for problems found on 12/14/21****
             trueB=true;
-            //alert("A length = " + $('body.tp-modal-open').length);
         }
-        $("body").removeClass("modal-open");
-        $(".redacted-overlay").remove();
-        $(".subscription-required").remove();
-        $(".subscriber-only").removeClass("hide");
+        $("body").removeClass("modal-open"); // this makes the page scrollable *****always check this before removing*****
+        $("div#asset-content").css("display", "block"); // this unhides text in body of page // ****updated in .54 for problems found on 12/14/21****
 
         counter++;
         if(trueA==true && trueB==true){ // sets clearInterval for when it completes sucessfully
             clearInterval (timerVar);
             timerVar = "";
-            //alert("complete, all true = " + counter);
         }
         if (counter==240){ // sets clearInterval time out for this number in seconds * 4 (for above 250 ms refresh)
             clearInterval (timerVar);
             timerVar = "";
-            //alert("timerVar counter done");
         }
-        //cant get javascript natural to work.
-        //         document.getElementsByClassName('tp-modal-open')[0].style.overflow = "scroll !important";
-        //         document.querySelectorAll("body.tp-modal-open")[0].style.overflow = "scroll !important";
-        //         document.querySelectorAll("body.tp-modal-open")[0].classList.remove(".tp-modal-open");
-        //         document.getElementsByClassName('tp-modal')[0].style.display = "none !important";
-        //         document.getElementsByClassName('tp-backdrop tp-active')[0].style.display = "none !important";
-        //         alert("test");
+
+        //$(".redacted-overlay").remove(); // removed 12/14/21 seemingly no longer relevant
+        //$(".subscription-required").remove(); // removed 12/14/21 seemingly no longer relevant
+        //$(".subscriber-only").removeClass("hide"); // removed 12/14/21 seemingly no longer relevant
 
     }
-
 })();
